af6ef4f79f970b853a56be8dd3f686ff053d3860
Anais <131727001+nanadess@users.noreply.github.com>
2025-02-08 17:28:49 +0100

    Fixing issue #27

--- code/public/main.js
+++ code/public/main.js
@@ -20,3 +20,35 @@ document.addEventListener("DOMContentLoaded", function() {
   // Add a class that triggers the fade-in effect
   document.body.classList.add('fade-transition-visible');
 });
+
+// project filter
+document.addEventListener('DOMContentLoaded', function () {
+  const tabs = document.querySelectorAll('[role="tab"]');
+  const projects = document.querySelectorAll('.container[data-type], hr[data-type]');
+
+  tabs.forEach(tab => {
+    tab.addEventListener('click', function () {
+      const type = this.getAttribute('data-type');
+
+      // Remove active class from all tabs
+      tabs.forEach(t => t.classList.remove('tab-active'));
+
+      // Add active class to the clicked tab
+      this.classList.add('tab-active');
+
+      // Filter projects based on the selected type with a delay for animation
+      projects.forEach(project => {
+        if (type === 'all' || project.getAttribute('data-type') === type) {
+          project.classList.remove('hide');
+          project.classList.add('show');
+        } else {
+          project.classList.add('hide');
+          project.classList.remove('show');
+        }
+      });
+    });
+  });
+});
+
+
+
