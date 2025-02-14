ba3966a035df326132796f1ce2111ca7ea0afb5c
Anais <131727001+nanadess@users.noreply.github.com>
2025-02-14 22:31:36 +0100

    Fixing issue #22
    lol, i had forgotten to remove the js script, duh

--- code/public/main.js
+++ code/public/main.js
@@ -1,20 +1,3 @@
-// navbar.js
-const hamburgerBtn = document.getElementById("hamburger-btn");
-const mobileMenu = document.getElementById("mobile-menu");
-const navbarLinks = document.getElementById("navbar-links");
-
-hamburgerBtn.addEventListener("click", () => {
-  mobileMenu.classList.toggle("hidden"); // Show/hide the mobile menu on button click
-  navbarLinks.classList.add("hidden");  // Hide the desktop links when hamburger is clicked
-});
-
-// hide the mobile menu if clicked outside
-window.addEventListener('click', function (event) {
-  if (!event.target.closest('#mobile-menu') && !event.target.closest('#hamburger-btn') && !event.target.closest('#navbar-links')) {
-    mobileMenu.classList.add('hidden');
-  }
-});
-
 // transition between pages
 document.addEventListener("DOMContentLoaded", function() {
   // Add a class that triggers the fade-in effect
